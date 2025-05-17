20c3b4adf01bbbc8a426b0df6fc425d3f3d9992c
alphappy <alphappyalphappy@gmail.com>
2025-05-17 13:46:55 -0500

    add basic instructions tab

--- Settings.cs
+++ Settings.cs
@@ -52,13 +52,17 @@ namespace alphappy.Watchermelon
             outputLabel = new OpLabelLong(new(400f, 550f), new(150f, 0f), "");
             routeLabel = new OpLabelLong(new(20f, 400f), new(350f, 0f), "");
 
-            Tabs = new OpTab[] { new(this, "Settings") };
+            Tabs = new OpTab[] { new(this, "Settings"), new(this, "Instructions") };
             Tabs[0].AddItems(
                 overrideBox, seedEntry,
                 runningBox, speedSplider,
                 button, button2,
                 outputLabel, routeLabel
                 );
+
+            OpLabelLong instructions = new(new(10f, 580f), new(580f, 0f), instructionText);
+
+            Tabs[1].AddItems(instructions);
         }
 
         public override void Update()
@@ -81,5 +85,28 @@ namespace alphappy.Watchermelon
         {
             return $"{ValidationString_ID()} {SeedText}";
         }
+
+        public static string instructionText =
+            "The route is defined by a sequence of steps with one step on each line.  There are two types of steps:\n\n" +
+
+            "   > A visit step looks like `VISIT: REGIONS` specifies that regions are being visited and Spinning Tops (STs) are being met.  `REGIONS` is a space-separated sequence of region codes.  If a ST is being met, an asterisk `*` is placed after that region.  Regions may be specified multiple times (for instance, to communicate the actual route) and this has no effect.\n" +
+            "   > A normal dynamic warp step looks like `NORMAL: SOURCE TARGET`.  `SOURCE` is the code of the region (or room) that the warp is occurring from.  `TARGET` is the specific room that the warp should go to.  Watchermelon does not currently verify that `TARGET` is valid for a given Ripple level.\n\n" +
+
+            "There are numerous factors which affect where a dynamic warp goes, and each of the route step types is intended to address these factors (though not every factor is accounted for yet):\n\n" +
+
+            "   > The Watcher campaign seed.  In 1.10.4, the Remix setting the Watcher Remix menu does not work.  The one in Watchermelon does.\n" +
+            "   > The number of dynamic warps already created.\n" +
+            "   > The current Ripple level.  Each potential target has a minimum Ripple requirement, but the weight of a target also decreases as Ripple goes beyond that requirement.\n" +
+            "   > The region that the warp is being created from.  A dynamic warp cannot target the region it originates from.\n" +
+            "   > Which regions have already been visited.  It's important that `VISIT` steps indicate every region that gets loaded, even if a shelter does not get used in that region.\n" +
+            "   > How many Prince encounters have occurred.\n" +
+            "   > Whether a dynamic warp could spread rot to an infectable region.\n\n" +
+
+            "Once a route has been created and saved with an external text editor, click Reload to import the route.  " +
+            "The second checkbox and slider may be used to adjust the search speed " +
+            "(set it too high and your game will probably become slow and unresponsive).  " +
+            "Once a seed has been found, use the first checkbox and numeric entry to use that seed.\n\n" +
+
+            "While Watchermelon will let you use (almost) any seed value for testing, note that only seeds 1 through 99,999 are legal in the Watcher Remix interface (whenever it actually gets fixed).  0 is a valid value for the Watcher Remix interface, but this just causes the game to pick a random seed when the campaign starts.";
     }
 }
